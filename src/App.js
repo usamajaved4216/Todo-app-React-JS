a99fc9b63c91797b64d99ae223bff9a10d3574d9
usamajaved4216@gmail.com <usamajaved4216@gmail.com>
2023-12-26 20:27:20 +0500

    continue

--- src/App.js
+++ src/App.js
@@ -3,79 +3,96 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [addValue, setaddValue] = useState ([]);
-  const [inpText, setinpText] = useState ();
-  const [editMode, seteditMode] = useState (false); // 1
-  const [ currentIndex, setcurrentIndex] =  useState();
+  const [addValue, setaddValue] = useState([]);
+  const [inpText, setinpText] = useState("");
+  const [editMode, seteditMode] = useState(false); // 1
+  const [currentIndex, setcurrentIndex] = useState();
 
 
-  function addItem(){
-    const copylist = [...addValue]
-    copylist.push(inpText)
-    setaddValue(copylist)
+  function addItem() {
+    if (inpText === '') {
+      alert('please enter value')
+    return
+    } 
+      const copylist = [...addValue]
+      copylist.push(inpText)
+      setaddValue(copylist)
+
+      setinpText("")
     
+
+
+  }
+  function changeValue(e) {
+
+    const value = e.target.value
+    setinpText(value)
+  }
+
+  function deleteItem(index) {
+    const copylist = [...addValue]
+    copylist.splice(index, 1)
+    setaddValue(copylist)
+
+  }
+  function editItem(index) {
+    const editBtn = addValue[index];
+    setinpText(editBtn)
+    seteditMode(true) //  1 edit pr click hoga tu add wala btn update me change hoga
+
+    setcurrentIndex(index)
+  }
+  function updatebtn() {
+    if (inpText === '') {
+      alert('please enter value')
+    return
+    } 
+
+    const updatebtn = [...addValue]
+    updatebtn[currentIndex] = inpText
+    setaddValue(updatebtn)
+    seteditMode(false)
     setinpText("")
-   }
-function changeValue(e){
 
-  const value = e.target.value
-  setinpText(value)
-}
+  }
+  function dltallItem() {
+    setaddValue([])
+  }
 
-function deleteItem(index){
-  const copylist = [...addValue]
-  copylist.splice(index, 1)
-  setaddValue(copylist)
-
-}
-function editItem(index){
-  const editBtn = addValue[index];
-  setinpText(editBtn)
-  seteditMode(true) //  1 edit pr click hoga tu add wala btn update me change hoga
-  
-setcurrentIndex(index)
-}
-function updatebtn (){
-  const updatebtn =[...addValue]
-  updatebtn[currentIndex] = inpText
-  setaddValue(updatebtn)
-seteditMode(false)
-setinpText("")
-
-}
-  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <div className='bgChange' >
 
-          <h1>Todo-App</h1>
+            <h1>Todo-App</h1>
 
-         <input onChange={changeValue} placeholder='Enter Any Value' value={inpText}  />
-         
-         {editMode ? <button onClick={updatebtn} >update</button>
-         :                                  // 1
-         <button onClick={addItem} >Add</button>}
+            <input className='place' onChange={changeValue} placeholder='Enter Any Value' value={inpText} />
 
-         <ul>
-          {addValue.map(function(item, index){
-            return <li>{item}
-            <button onClick={ () => deleteItem (index)} > Delete</button>
-            <button  onClick={ () => editItem (index)} > edit</button>
+            {editMode ? <button className='placeUpdate' onClick={updatebtn} >update</button>
+              :                                  // 1
+              <button className='placeAdd' onClick={addItem} >Add</button>}
 
-            </li>
-          })}
-         </ul>
+
+            <button className='placeDlt ' onClick={dltallItem} >delete all</button>
+
+
+
+
+
+
+            <ul >
+              {addValue.map(function (item, index) {
+                return <li className={editMode && currentIndex === index ? 'li' : ''}>{item}
+                  <button className='dleteBtn' onClick={() => deleteItem(index)} > Delete</button>
+                  <button className='editBtn' onClick={() => editItem(index)} > Edit</button>
+
+                </li>
+              })}
+            </ul>
+          </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
